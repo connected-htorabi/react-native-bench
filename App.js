e39156e4f57bfcd2fbb0e8eae7e09a73da7aa38b
connected-ndhindsa <ndhindsa@connected.io>
2021-11-04 09:44:19 -0400

    Remove separate components and add to Wallet

--- App.js
+++ App.js
@@ -1,45 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Expandable from "./components/Expandable";
-import Header from "./components/Header";
-import Body from "./components/Body";
-import Icon from "./components/Icon";
-import Payee from "./components/Payee";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 
-const info = [
-  { header: "First Header", note: "First Note" },
-  { header: "Second Header", note: "Second Note" },
-  { header: "Third Header", note: "Third Note" },
-];
+import Wallet from "./components/Wallet";
 
 export default function App() {
-  const [activeIndex, setActiveIndex] = useState(null);
-
-  const onExpand = (index) => {
-    setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
-  };
-
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      {info.map((item, index) => (
-        <Expandable
-          shouldExpand={index === activeIndex}
-          onExpand={() => onExpand(index)}
-          key={index}
-        >
-          <Header dataIndex={index}>
-            <Text>{item.header}</Text>
-            <Icon />
-          </Header>
-
-          <Body>
-            <Text>{item.note}</Text>
-          </Body>
-        </Expandable>
-      ))}
-      <Payee name="Haydn" />
+      <Wallet />
     </View>
   );
 }
@@ -50,10 +19,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "flex-start",
-  },
-  header: {
-    backgroundColor: "#fcc",
-    width: 200,
-    height: 100,
   },
 });
