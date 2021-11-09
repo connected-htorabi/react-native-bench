730087bb8967a655154bd4dd6bb92c5fe2a7b6c8
Tyler Caceres <tcaceres@connected.io>
2021-11-09 16:11:41 -0500

    interim process

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -22,63 +22,33 @@ const IconMapping = {
 };
 
 export default function App() {
-  const [stackType, setStackType] = useState(true);
   return (
     <NavigationContainer>
-      <Button
-        title="toggle navigation"
-        onPress={() => setStackType(!stackType)}
-      ></Button>
-      {stackType ? (
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ color, size }) => (
+      <Tab.Navigator
+        screenOptions={({ route, navigation }) => {
+          return {
+            tabBarActiveTintColor: "black",
+            headerTitleAlign: "center",
+            headerRight: () => (
+              <HeaderRight navigation={navigation} route={route} />
+            ),
+            headerLeft: () => <HeaderLeft navigation={navigation} />,
+            tabBarIcon: ({ color, size, focused }) => (
               <MaterialCommunityIcons
                 name={IconMapping[route.name]}
                 color={color}
                 size={size}
               />
             ),
-          })}
-        >
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Browse" component={() => <Text>Browse</Text>} />
-          <Tab.Screen name="Grocery" component={() => <Text>Grocery</Text>} />
-          <Tab.Screen name="Orders" component={() => <Text>Orders</Text>} />
-          <Tab.Screen name="Account" component={() => <Text>Account</Text>} />
-        </Tab.Navigator>
-      ) : (
-        <Stack.Navigator
-          initialRouteName="Home"
-          screenOptions={({ route, navigation }) => ({
-            headerTitleAlign: "center",
-            headerRight: () => (
-              <HeaderRight navigation={navigation} route={route} />
-            ),
-            headerLeft: () => <HeaderLeft navigation={navigation} />,
-          })}
-        >
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{
-              title: "Home",
-            }}
-          />
-          <Stack.Screen
-            name="Wallet"
-            component={Wallet}
-            options={{
-              title: "Wallet",
-            }}
-          />
-          <Stack.Screen
-            name="About"
-            component={About}
-            options={{ title: "About" }}
-          />
-        </Stack.Navigator>
-      )}
+          };
+        }}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Browse" component={() => <Text>Browse</Text>} />
+        <Tab.Screen name="Grocery" component={() => <Text>Grocery</Text>} />
+        <Tab.Screen name="Orders" component={() => <Text>Orders</Text>} />
+        <Tab.Screen name="Account" component={() => <Text>Account</Text>} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
