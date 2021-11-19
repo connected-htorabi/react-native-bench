e2ac3830c0260142883f7a2edc6ae4e382a12492
Tyler Caceres <tcaceres@connected.io>
2021-11-19 18:12:42 -0500

    initialize swipe in the list

--- components/cart/order/OrderSection.jsx
+++ components/cart/order/OrderSection.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FlatList } from 'react-native';
+import { FlatList, Text } from 'react-native';
+import { SwipeListView } from 'react-native-swipe-list-view';
 
 import OrderItem, { OrderItemPropTypes } from './OrderItem';
 import ItemSeparator from '../ItemSeparator';
 
 const OrderSection = ({ items = [] }) => (
-    <FlatList
-        data={items}
-        renderItem={({ item }) => <OrderItem {...item} />}
-        keyExtractor={(item) => item.id}
-        ItemSeparatorComponent={ItemSeparator}
-        scrollEnabled={false}
-        // TODO - figure out why this is required
-        style={{ flexGrow: 0 }}
-    />
+    <>
+        <FlatList
+            data={items}
+            renderItem={({ item }) => <OrderItem {...item} />}
+            keyExtractor={(item) => item.id}
+            ItemSeparatorComponent={ItemSeparator}
+            scrollEnabled={false}
+            // TODO - figure out why this is required
+            style={{ flexGrow: 0 }}
+        />
+        <SwipeListView
+            data={items}
+            renderItem={({ item }) => <OrderItem {...item} />}
+            renderHiddenItem={({ item }) => (
+                <Text style={{ textAlign: 'right' }}>Hello there</Text>
+            )}
+            rightOpenValue={-20}
+        />
+    </>
 );
 
 OrderSection.propTypes = {
