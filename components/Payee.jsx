a926b6c0fe47079ec32036b0da6fff782c13e07d
Haydn Wiese <hwiese@connected.io>
2021-12-02 12:04:21 -0500

    Rename Payee component to use JSX extension. Center text for "add" and "cancel"

--- components/Payee.jsx
+++ components/Payee.jsx
@@ -69,6 +69,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     addButton: {
+        alignItems: 'center',
         borderWidth: 1,
         borderRadius: 2,
         paddingTop: 5,
