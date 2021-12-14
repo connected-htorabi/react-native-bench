ef1be61afa5597845e33451c15eabe09825413d1
Tyler Caceres <tcaceres@connected.io>
2021-12-14 09:58:50 -0500

    cleanup App file

--- App.jsx
+++ App.jsx
@@ -1,23 +1,12 @@
 import React, { useEffect } from 'react';
-import { Provider, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Host } from 'react-native-portalize';
 
 import { fetchRestaurants } from './redux/thunks/fetchRestaurants';
 import AppStack from './navigation/Home/AppStack';
 import { useGetUserQuery } from './redux/services/restaurant';
-
-const Tab = createBottomTabNavigator();
-
-const IconMapping = {
-    Grocery: 'basket',
-    Home: 'home',
-    Browse: 'text-search',
-    Orders: 'receipt',
-    Account: 'account',
-};
 
 const App = () => {
     const dispatch = useDispatch();
@@ -31,32 +20,6 @@ const App = () => {
         <NavigationContainer>
             <Host>
                 <AppStack />
-                {/* <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={({ route }) => {
-          return {
-            tabBarActiveTintColor: "black",
-            headerTitleAlign: "center",
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name={IconMapping[route.name]}
-                color={color}
-                size={size}
-              />
-            ),
-          };
-        }}
-      >
-        <Tab.Screen
-          name="Home"
-          component={HomeStack}
-          options={{ headerShown: false }}
-        />
-        <Tab.Screen name="Browse" component={() => <Text>Browse</Text>} />
-        <Tab.Screen name="Grocery" component={() => <Text>Grocery</Text>} />
-        <Tab.Screen name="Orders" component={() => <Text>Orders</Text>} />
-        <Tab.Screen name="Account" component={() => <Text>Account</Text>} />
-      </Tab.Navigator> */}
             </Host>
         </NavigationContainer>
     );
