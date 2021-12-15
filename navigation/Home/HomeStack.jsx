d74d67edfb07055c84c99827fa6292ddd49bfbc2
Tyler Caceres <tcaceres@connected.io>
2021-12-15 13:27:59 -0500

    navigation and header component cleanup

--- navigation/Home/HomeStack.jsx
+++ navigation/Home/HomeStack.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { HeaderRight, HeaderLeft } from '../../components/NavHeader';
-import Wallet from '../../screens/Wallet';
 import HomeScreen from '../../screens/HomeScreen';
 import RestaurantDetails from '../../screens/RestaurantDetails';
 import ItemDetails from '../../screens/ItemDetails';
@@ -14,21 +13,18 @@ const Stack = createNativeStackNavigator();
 export const HomeStack = () => (
     <Stack.Navigator
         initialRouteName="Home"
-        screenOptions={({ route, navigation }) => ({
+        screenOptions={{
             headerTitleAlign: 'center',
-            headerRight: () => (
-                <HeaderRight navigation={navigation} route={route} />
-            ),
-        })}
+            headerRight: HeaderRight,
+        }}
     >
         <Stack.Screen
             name="Home"
             component={HomeScreen}
-            options={({ navigation }) => ({
-                headerLeft: () => <HeaderLeft navigation={navigation} />,
-            })}
+            options={{
+                headerLeft: HeaderLeft,
+            }}
         />
-        <Stack.Screen name="Wallet" component={Wallet} />
         <Stack.Screen
             name="Cart"
             component={Cart}
